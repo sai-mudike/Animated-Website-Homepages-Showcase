d4d796d01e3a7682e0f5e7cc5bca30493546a423
Sainitin Mudike <127184650+sai-mudike@users.noreply.github.com>
2023-08-13 18:26:30 +0530

    Update app.js

--- sidcupfamilygolf website/app.js
+++ sidcupfamilygolf website/app.js
@@ -79,15 +79,3 @@ gsap.from(".page6 h1", {
     }
 
 })
-
-👋 Hi there!
-
-I've got something cool in my GitHub repository. It's like a treasure chest where I've recreated a webpage from a place called Sidcup Family Golf 🌐. You know, it's like making your own version of a puzzle!
-
-To make it all happen, I used some special tools like HTML, CSS, and a bit of JavaScript.These are like the building blocks that make websites look and behave in certain ways.
-
-Feel free to take a look at what I've done. You'll find how I made things move and look interesting on the page.Remember, this is a way for me to learn more about how websites work 💻🚀.
-
-If you're curious about the original inspiration, check out the Sidcup Family Golf website here: https://sidcupfamilygolf.com/.
-
-Have fun exploring, and feel free to drop by anytime to see what I've created! 🏌️‍♂️🎨
